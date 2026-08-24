91a6eb941fc8d7ea905c475c155b598b57f7a8d5
EliWoodard <elijahwoodard136@gmail.com>
2026-08-23 22:50:37 -0700

    Created Calculator Project

---
+++ calculator.py
@@ -0,0 +1,9 @@
+print("===== Calculator =====")
+
+userFirstInput = float(input("Enter first number:"))
+userSecondInput = float(input("Enter second number:"))
+
+print(f"Sum: {userFirstInput + userSecondInput}")
+print(f"Difference: {userFirstInput - userSecondInput}")
+print(f"Multiplication: {userFirstInput * userSecondInput}")
+print(f"Division: {userFirstInput / userSecondInput}")
